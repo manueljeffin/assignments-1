4f31531697623e083202ec578f61ff2e2598ca85
akash227 <akash227@yahoo.com>
2016-01-26 15:46:08 +0530

    Adding countthebits.c

---
+++ countthebits.c
@@ -0,0 +1,28 @@
+/**
+*
+* This program calculates the number of bits required to represent a decimal number 
+*
+**/
+
+#include <stdio.h>
+
+int countthebits(unsigned long long number){
+	int count=0;
+	while (number > 0){
+		number = number/2;
+		count++;
+	}
+	return count;
+}
+
+
+
+int main(){
+	unsigned long long n;
+	int num;
+	printf("Enter the decimal number: ");
+	scanf("%llu",&n);
+	num  = countthebits(n);
+	printf("Number of digits required : %d\n",num);
+	return 0;
+}
