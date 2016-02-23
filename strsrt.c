6deb3fe17bc15fb3730844e3c872cd90731b11c0
akash227 <akash227@yahoo.com>
2016-02-23 19:06:42 +0530

    Mod strsrt.c

--- strsrt.c
+++ strsrt.c
@@ -1,38 +1,57 @@
 #include <stdio.h>
 
 int main(){
-	char *str[] = {"1", "3", "11","21","12", "100", "19", "2", "5"},*temp;
-	int i,j,m;
-	m=9;
-	printf("Before sorting...\n");
-	int a[m],tmp;
-	for(i=0;i<m;i++){
-		a[i]=0;
-		printf("%s ",str[i] );
-	}
-	printf("\n");
-	for(i=0;i<m;i++){
-		for(j=0;str[i][j]!='\0';j++){
-			a[i] = str[i][j]+a[i]*10; 
-			//since  '+' and '*' are commutative...'21' & '12' will be the same after performing any of those operations
-		}
-	}
-	for(i=0;i<m;i++){
-		for(j=i;j<m;j++){
-			if(a[i]>a[j]){
-				 temp = str[i];
-				 str[i] = str[j];
-				 str[j] = temp;
-				 tmp  = a[i];
-				 a[i] = a[j];
-				 a[j] = tmp;
-			}
-		}
-	}
-	printf("After sorting...\n");
-	for(i=0;i<m;i++){
-		printf("%s ",str[i] );
-	}
-	printf("\n");
-	return 0;
+    //char *str[9] = {"1", "3", "11","21","12", "100", "19", "2", "5"},*temp;
+    char *str[9] = {"1234","412","12","7","5","21","12","1","99"},*temp;
+    int i,j,k,r,mazSize,m;
+    m = 9; //Number of Strings
+    mazSize = 4; //Maximum length of a string
+    printf("Before sorting...\n");
+    for(i=0;i<m;i++){
+        printf("%s ", str[i] );
+    }
+    printf("\n");
+    int a[m],tmp,prevK;
+    for(i=0;i<m;i++){
+        a[i]=1;
+    }
+    for(k=0;k<mazSize;k++){
+        prevK = (k >= 1 )? k-1 : 0;
+        for(i=0;i<m;i++){
+            for(j=0;j<(m-1);j++){
+                if( a[j] ){
+                    if( str[j][k]== '\0' ){
+                        a[j]=0;
+                        break;
+                    }
+                    if (str[j+1][k]=='\0'){
+                        a[j+1]=0;
+                    }
+                    if(a[j]>a[j+1]){
+                        temp=str[j];
+                        str[j]=str[j+1];
+                        str[j+1]=temp;
+                        tmp=a[j];
+                        a[j]=a[j+1];
+                        a[j+1]=tmp;
+                    }else if(str[j][prevK] > str[j+1][prevK]){
+                        temp=str[j];
+                        str[j]=str[j+1];
+                        str[j+1]=temp;
+                    }else if( str[j][prevK] == str[j+1][prevK]&& str[j][k] > str[j+1][k]){
+                        temp=str[j];
+                        str[j]=str[j+1];
+                        str[j+1]=temp;
+                    }
+                }
+            }
+            
+        }
+    }
+    printf("After Sorting...\n");
+    for(i=0;i<m;i++){
+        printf("%s ", str[i]);
+    }
+    printf("\n");
+    return 0;
 }
